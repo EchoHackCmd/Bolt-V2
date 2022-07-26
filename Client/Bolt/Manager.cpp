c7c7a35b22542a3802868ce62827eca6d6eda645
EchoHackCmd <33047114+EchoHackCmd@users.noreply.github.com>
2022-07-26 19:57:45 +0100

    Update Manager.cpp

--- Client/Bolt/Manager.cpp
+++ Client/Bolt/Manager.cpp
@@ -1,20 +1,34 @@
 #include "Manager.h"
-
+#include "Category/Category.h"
 
 Manager::Manager(Client* client) {
 	this->client = client;
+
 	this->initHooks();
+
+	this->initCategories();
 	this->initModules();
 };
 
 auto Manager::initModules(void) -> void {
+	
 	//
-}
+
+};
 
 auto Manager::initHooks(void) -> void {
+	
 	//
-}
+
+};
 
 auto Manager::initCategories(void) -> void {
-	//
-}
+	
+	for(auto First = (int)CategoryType::Combat; First <= (int)CategoryType::World; First++) {
+
+		CategoryType currType = static_cast<CategoryType>(First);
+		this->categories[currType] = new Category(currType, this);
+
+	};
+
+};
