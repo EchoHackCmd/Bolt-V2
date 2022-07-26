96cf796b629ceaa219618970de58fbb3b01a61e8
Xello12121 <82851468+Xello12121@users.noreply.github.com>
2022-07-26 16:10:17 -0500

    sig scanner

--- Client/Util/Util.cpp
+++ Client/Util/Util.cpp
@@ -2,48 +2,98 @@
 
 auto Util::getDll(void) -> HMODULE {
 
-    MEMORY_BASIC_INFORMATION info;
-    size_t len = VirtualQueryEx(GetCurrentProcess(), (void*)getDll, &info, sizeof(info));
-    assert(len == sizeof(info));
-    return len ? (HMODULE)info.AllocationBase : NULL;
+	MEMORY_BASIC_INFORMATION info;
+	size_t len = VirtualQueryEx(GetCurrentProcess(), (void*)getDll, &info, sizeof(info));
+	assert(len == sizeof(info));
+	return len ? (HMODULE)info.AllocationBase : NULL;
 
 };
 
 auto Util::getDebugDir(void) -> std::string {
 
-    char* path = NULL;
-    size_t length;
+	char* path = NULL;
+	size_t length;
 
-    _dupenv_s(&path, &length, "appdata");
+	_dupenv_s(&path, &length, "appdata");
 
-    return std::string(path);
+	return std::string(path);
 
 };
 
 auto Util::debugLog(std::string output) -> void {
 
-    static bool init = false;
-    static auto path = std::string(getDebugDir() + "\\..\\Local\\Packages\\Microsoft.MinecraftUWP_8wekyb3d8bbwe\\RoamingState");
+	static bool init = false;
+	static auto path = std::string(getDebugDir() + "\\..\\Local\\Packages\\Microsoft.MinecraftUWP_8wekyb3d8bbwe\\RoamingState");
 
-    auto file = std::string(path + "\\Output.txt");
+	auto file = std::string(path + "\\Output.txt");
 
-    if(!init) {
-        auto f = std::filesystem::path(file);
+	if (!init) {
+		auto f = std::filesystem::path(file);
 
-        if(std::filesystem::exists(f))
-            std::filesystem::remove(f);
-        
-        init = true;
-    };
-    
-    CloseHandle(CreateFileA(file.c_str(), GENERIC_WRITE | GENERIC_READ, FILE_SHARE_READ | FILE_SHARE_WRITE, NULL, OPEN_ALWAYS, FILE_ATTRIBUTE_NORMAL, NULL));
+		if (std::filesystem::exists(f))
+			std::filesystem::remove(f);
 
-    std::ofstream fStream;
-    fStream.open(file.c_str(), std::ios::app);
-    
-    if(fStream.is_open())
-        fStream << output << "\n" << std::endl;
-    
-    return fStream.close();
+		init = true;
+	};
+
+	CloseHandle(CreateFileA(file.c_str(), GENERIC_WRITE | GENERIC_READ, FILE_SHARE_READ | FILE_SHARE_WRITE, NULL, OPEN_ALWAYS, FILE_ATTRIBUTE_NORMAL, NULL));
+
+	std::ofstream fStream;
+	fStream.open(file.c_str(), std::ios::app);
+
+	if (fStream.is_open())
+		fStream << output << "\n" << std::endl;
+
+	return fStream.close();
 
 };
+
+auto Util::FindSig(const char* szSignature) -> uintptr_t {
+	const char* pattern = szSignature;
+	uintptr_t firstMatch = 0;
+	static const uintptr_t rangeStart = (uintptr_t)GetModuleHandleA("Minecraft.Windows.exe");
+	MODULEINFO miModInfo;
+	static bool init = false;
+	if (!init) {
+		init = true;
+		GetModuleInformation(GetCurrentProcess(), (HMODULE)rangeStart, &miModInfo, sizeof(MODULEINFO));
+	}
+	static const uintptr_t rangeEnd = rangeStart + miModInfo.SizeOfImage;
+
+	BYTE patByte = GET_BYTE(pattern);
+	const char* oldPat = pattern;
+
+	for (uintptr_t pCur = rangeStart; pCur < rangeEnd; pCur++)
+	{
+		if (!*pattern)
+			return firstMatch;
+
+		while (*(PBYTE)pattern == ' ')
+			pattern++;
+
+		if (!*pattern)
+			return firstMatch;
+
+		if (oldPat != pattern) {
+			oldPat = pattern;
+			if (*(PBYTE)pattern != '\?')
+				patByte = GET_BYTE(pattern);
+		}
+
+		if (*(PBYTE)pattern == '\?' || *(BYTE*)pCur == patByte)
+		{
+			if (!firstMatch)
+				firstMatch = pCur;
+
+			if (!pattern[2])
+				return firstMatch;
+
+			pattern += 2;
+		}
+		else
+		{
+			pattern = szSignature;
+			firstMatch = 0;
+		}
+	}
+}
