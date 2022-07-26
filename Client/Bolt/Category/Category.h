e02e167f46a55644d909b87144f8eeb9897d2c8c
EchoHackCmd <33047114+EchoHackCmd@users.noreply.github.com>
2022-07-26 21:20:09 +0100

    Update Category.h

--- Client/Bolt/Category/Category.h
+++ Client/Bolt/Category/Category.h
@@ -1,21 +1,21 @@
 #ifndef CLIENT_BOLT_CATEGORY_CATEGORY
 #define CLIENT_BOLT_CATEGORY_CATEGORY
 
-#include "../../Util/Util.h"
+#include "../Module/Module.h"
 
 class Manager;
-class Module;
 
 enum CategoryType {
     Combat = 0,
     Movement = 1,
     Player = 2,
     Render = 3,
-    World = 4
+    World = 4,
+    Other = 5
 };
 
 class Category {
-private:
+public:
     CategoryType type;
     Manager* manager;
 public:
