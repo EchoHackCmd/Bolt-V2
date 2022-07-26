44e3985eb52c6783950e65efe262074f1134c61b
EchoHackCmd <33047114+EchoHackCmd@users.noreply.github.com>
2022-07-26 22:43:16 +0100

    Update TestModule.cpp

--- Client/Bolt/Module/Other/TestModule.cpp
+++ Client/Bolt/Module/Other/TestModule.cpp
@@ -1,11 +1,14 @@
 #include "TestModule.h"
 
+#include "../../Category/Category.h"
+#include "../../Manager.h"
+
 auto TestModule::onTick(void) -> void {
 
     this->tickInt++;
 
-    if(this->tickInt > 100)
-        return this->setState(false);
+    if(this->tickInt >= 100)
+        this->category->manager->isRunning = false;
     
     Util::debugLog( std::string( "Tick: " + std::to_string( this->tickInt ) ) );
 
