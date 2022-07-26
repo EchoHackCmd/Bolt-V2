c3cb3f4379b5582dfea5a79c7f95c724bc91a8fd
EchoHackCmd <33047114+EchoHackCmd@users.noreply.github.com>
2022-07-26 19:10:12 +0100

    Update Util.cpp

--- Client/Util/Util.cpp
+++ Client/Util/Util.cpp
@@ -1,1 +1,10 @@
 #include "Util.h"
+
+auto Util::getDll(void) -> HMODULE {
+
+    MEMORY_BASIC_INFORMATION info;
+    size_t len = VirtualQueryEx(GetCurrentProcess(), (void*)getDll, &info, sizeof(info));
+    assert(len == sizeof(info));
+    return len ? (HMODULE)info.AllocationBase : NULL;
+
+};
