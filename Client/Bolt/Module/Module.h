f049edf2a794a3a8ed421240113259195b96168b
EchoHackCmd <33047114+EchoHackCmd@users.noreply.github.com>
2022-07-26 21:20:03 +0100

    Update Module.h

--- Client/Bolt/Module/Module.h
+++ Client/Bolt/Module/Module.h
@@ -1,9 +1,16 @@
 #ifndef CLIENT_BOLT_MODULE_MODULE
 #define CLIENT_BOLT_MODULE_MODULE
 
+#include "../../Util/Util.h"
+
+class Category;
+
 class Module {
 public:
-    //
+    std::string name;
+    Category* category;
+public:
+    Module(std::string, Category*);
 };
 
 #endif /* CLIENT_BOLT_MODULE_MODULE */
