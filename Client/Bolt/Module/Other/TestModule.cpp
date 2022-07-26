71a4ff90c8eba3800a042ebbde7849cbaaf2aa3b
EchoHackCmd <33047114+EchoHackCmd@users.noreply.github.com>
2022-07-26 21:37:44 +0100

    Update TestModule.cpp

--- Client/Bolt/Module/Other/TestModule.cpp
+++ Client/Bolt/Module/Other/TestModule.cpp
@@ -2,6 +2,11 @@
 
 auto TestModule::onTick(void) -> void {
 
-    Util::debugLog("Tick: " + this->name);
+    this->tickInt++;
+
+    if(this->tickInt > 100)
+        return this->setState(false);
+    
+    Util::debugLog( std::string( "Tick: " + std::to_string( this->tickInt ) ) );
 
 };
