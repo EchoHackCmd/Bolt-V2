6a52323721648fc86a0e0ac0142f04c4fc118802
EchoHackCmd <33047114+EchoHackCmd@users.noreply.github.com>
2022-07-26 20:03:45 +0100

    Update Category.cpp

--- Client/Bolt/Category/Category.cpp
+++ Client/Bolt/Category/Category.cpp
@@ -1,8 +1,21 @@
 #include "Category.h"
+#include "../Manager.h"
 
 Category::Category(CategoryType type, Manager* mgr) {
 
     this->type = type;
     this->manager = mgr;
+
+};
+
+auto Category::getName(void) -> std::string {
+
+    auto mgr = this->manager;
+    auto res = std::string("Unknown");
+
+    if(mgr == nullptr)
+        return res;
+    
+    return mgr->getCategory(this->type);
 
 };
