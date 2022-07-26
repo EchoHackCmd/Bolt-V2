a93167642d0aef299afb32b1195bd793c9928706
EchoHackCmd <33047114+EchoHackCmd@users.noreply.github.com>
2022-07-26 18:25:53 +0100

    Update Bolt.cpp

--- Bolt.cpp
+++ Bolt.cpp
@@ -1,5 +1,25 @@
-#include <iostream>
+#include "Client/Client.h"
+#include <Windows.h>
 
-void say_hello(){
-    std::cout << "Hello, from Bolt!\n";
-}
+auto init(HMODULE hMod) -> void {
+
+    auto client = new Client("Bolt-V2");
+
+    Sleep(5000);
+    FreeLibraryAndExitThread(hMod, 1);
+
+};
+
+BOOL WINAPI DllMain(HINSTANCE hInstance, DWORD fdwReason, LPVOID lpRes) {
+
+    switch(fdwReason) {
+
+        case DLL_PROCESS_ATTACH:
+            CreateThread(0, 0, (LPTHREAD_START_ROUTINE)init, hInstance, 0, 0);
+        break;
+
+    };
+
+    return TRUE;
+
+};
