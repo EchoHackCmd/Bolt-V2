c62a3ab9125aaf6874694f819947f90dac073f72
EchoHackCmd <33047114+EchoHackCmd@users.noreply.github.com>
2022-07-26 21:35:22 +0100

    Update Module.h

--- Client/Bolt/Module/Module.h
+++ Client/Bolt/Module/Module.h
@@ -10,7 +10,17 @@ public:
     std::string name;
     Category* category;
 public:
+    uint64_t key;
+    bool isEnabled = false, wasEnabled = false;
+public:
     Module(std::string, Category*);
+public:
+    auto setState(bool) -> void;
+    auto setKey(uint64_t) -> void;
+public:
+    virtual auto onTick(void) -> void {};
+    virtual auto onEnable(void) -> void {};
+    virtual auto onDisable(void) -> void {};
 };
 
 #endif /* CLIENT_BOLT_MODULE_MODULE */
