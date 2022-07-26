fa1f6b36081296eedb2e81e70bea94dfef6c7c49
EchoHackCmd <33047114+EchoHackCmd@users.noreply.github.com>
2022-07-26 22:49:00 +0100

    Update TestModule.cpp

--- Client/Bolt/Module/Other/TestModule.cpp
+++ Client/Bolt/Module/Other/TestModule.cpp
@@ -7,8 +7,25 @@ auto TestModule::onTick(void) -> void {
 
     this->tickInt++;
 
-    if(this->tickInt >= 100)
+    if(this->tickInt >= 100) {
+
+        auto mgr = this->category->manager;
+
+        for(auto [ type, category ] : mgr->categories) {
+
+            auto cat = mgr->getCategory(type).second;
+
+            if(cat == nullptr)
+                Util::debugLog(category->getName());
+            else
+                Util::debugLog(cat->getName());
+
+        };
+
+        this->setState(false);
         this->category->manager->isRunning = false;
+
+    };
     
     Util::debugLog( std::string( "Tick: " + std::to_string( this->tickInt ) ) );
 
