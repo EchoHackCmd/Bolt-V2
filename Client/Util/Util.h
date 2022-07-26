824a01ecdc45e5229ce97db55608b8c489dd6aa2
EchoHackCmd <33047114+EchoHackCmd@users.noreply.github.com>
2022-07-26 19:13:30 +0100

    Update Util.h

--- Client/Util/Util.h
+++ Client/Util/Util.h
@@ -1,13 +1,18 @@
 #ifndef CLIENT_UTIL_UTIL
 #define CLIENT_UTIL_UTIL
 
+#include <filesystem>
 #include <Windows.h>
 #include <assert.h>
+#include <fstream>
 #include <string>
 
 class Util {
 public:
     static auto getDll(void) -> HMODULE;
+    static auto getDebugDir(void) -> std::string;
+public:
+    static auto debugLog(std::string) -> void;
 };
 
 #endif /* CLIENT_UTIL_UTIL */
