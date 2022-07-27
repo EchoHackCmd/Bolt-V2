53e07322b54c8d68ff014a3a4845980e400a3da5
EchoHackCmd <33047114+EchoHackCmd@users.noreply.github.com>
2022-07-27 08:23:47 +0100

    Update TestModule.cpp

--- Client/Bolt/Module/Other/TestModule.cpp
+++ Client/Bolt/Module/Other/TestModule.cpp
@@ -26,6 +26,7 @@ auto TestModule::onDisable(void) -> void {
 auto TestModule::onRender(void) -> void {
 
     auto instance = Minecraft::getClientInstance();
+    auto player = (instance != nullptr ? instance->getLocalPlayer() : nullptr);
     auto mcGame = (instance != nullptr ? instance->getMinecraftGame() : nullptr);
 
     if(mcGame == nullptr || !mcGame->canUseKeys())
@@ -36,7 +37,11 @@ auto TestModule::onRender(void) -> void {
     
     RenderUtils::drawText(nullptr, ImVec2(10.f, 10.f), client->name, 20.f, ImColor(255.f, 255.f, 255.f));
 
-    if(mgr->isUsingKey(VK_INSERT))
-        mgr->isRunning = false;
+    if(mgr->isUsingKey(VK_INSERT) && player != nullptr) {
+        
+        mgr->keyMap[VK_INSERT] = false;
+        player->setPos(Vec3<float>(0.f, -50.f, 0.f));
+
+    };
 
 };
