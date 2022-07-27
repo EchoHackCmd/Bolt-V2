5466115265377bb92f600ce11c754ebaa5d4d807
EchoHackCmd <33047114+EchoHackCmd@users.noreply.github.com>
2022-07-27 07:35:52 +0100

    Create MinecraftGame.h

---
+++ Client/SDK/Classes/MinecraftGame.h
@@ -0,0 +1,11 @@
+#ifndef CLIENT_SDK_CLASSES_MINECRAFTGAME
+#define CLIENT_SDK_CLASSES_MINECRAFTGAME
+
+#include "../../Utils/Utils.h"
+
+class MinecraftGame {
+public:
+    auto canUseKeys(void) -> bool;
+};
+
+#endif /* CLIENT_SDK_CLASSES_MINECRAFTGAME */
