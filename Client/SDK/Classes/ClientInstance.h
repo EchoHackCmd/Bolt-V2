e1ff37d2bead452dab82b2a331b61df4a0f9a3d1
EchoHackCmd <33047114+EchoHackCmd@users.noreply.github.com>
2022-07-27 05:36:26 +0100

    Update ClientInstance.h

--- Client/SDK/Classes/ClientInstance.h
+++ Client/SDK/Classes/ClientInstance.h
@@ -1,9 +1,14 @@
 #ifndef CLIENT_SDK_CLASSES_CLIENTINSTANCE
 #define CLIENT_SDK_CLASSES_CLIENTINSTANCE
 
+#include "../../Utils/Utils.h"
+
 class ClientInstance {
 public:
-    //
+    uintptr_t** VTable;
+public:
+    auto grabMouse(void) -> void;
+    auto releaseMouse(void) -> void;
 };
 
 #endif /* CLIENT_SDK_CLASSES_CLIENTINSTANCE */
