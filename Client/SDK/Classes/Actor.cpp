92fe3d7219d2d888741554138ef50ed5cc78ea09
EchoHackCmd <33047114+EchoHackCmd@users.noreply.github.com>
2022-07-27 08:23:44 +0100

    Update Actor.cpp

--- Client/SDK/Classes/Actor.cpp
+++ Client/SDK/Classes/Actor.cpp
@@ -5,3 +5,12 @@ auto Actor::onGround(void) -> bool* {
     return (bool*)((uintptr_t)(this) + 0x1D8);
     
 };
+
+auto Actor::setPos(Vec3<float> pos) -> void {
+
+    using SetPos = void (__thiscall*)(Actor*, Vec3<float>*);
+    auto _SetPos = (SetPos)(this->VTable[19]);
+
+    _SetPos(this, &pos);
+
+};
