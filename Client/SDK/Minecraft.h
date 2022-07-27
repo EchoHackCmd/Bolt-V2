7853fa5b6cbe613e2bc474b001f0f66aee543af6
EchoHackCmd <33047114+EchoHackCmd@users.noreply.github.com>
2022-07-27 02:20:54 +0100

    Update Minecraft.h

--- Client/SDK/Minecraft.h
+++ Client/SDK/Minecraft.h
@@ -1,9 +1,14 @@
 #ifndef CLIENT_SDK_MINECRAFT
 #define CLIENT_SDK_MINECRAFT
 
+#include "Classes/ClientInstance.h"
+
 class Minecraft {
+private:
+    static ClientInstance* clientInstance;
 public:
-    //
+    static auto setClientInstance(ClientInstance*) -> void;
+    static auto getClientInstance(void) -> ClientInstance*;
 };
 
 #endif /* CLIENT_SDK_MINECRAFT */
