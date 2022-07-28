ca8406ae87b33208b634fcb3b5193a7ab032939b
EchoHackCmd <33047114+EchoHackCmd@users.noreply.github.com>
2022-07-28 04:25:57 +0100

    Update Actor.cpp

--- Client/SDK/Classes/Actor.cpp
+++ Client/SDK/Classes/Actor.cpp
@@ -6,6 +6,10 @@ auto Actor::onGround(void) -> bool* {
     
 };
 
+
+//
+
+
 auto Actor::setPos(Vec3<float> pos) -> void {
 
     using SetPos = void (__thiscall*)(Actor*, Vec3<float>*);
@@ -21,5 +25,106 @@ auto Actor::getPos(void) -> Vec3<float> {
     auto _GetPos = (GetPos)(this->VTable[22]);
 
     return *_GetPos(this);
+
+};
+
+
+//
+
+
+auto Actor::getFiringPos(void) -> Vec3<float> {
+
+    using GetFiringPos = Vec3<float> (__thiscall*)(Actor*);
+    auto _GetFiringPos = (GetFiringPos)(this->VTable[26]);
+
+    return _GetFiringPos(this);
+
+};
+
+auto Actor::setRot(Vec2<float> rot) -> void {
+
+    using SetRot = void (__thiscall*)(Actor*, Vec2<float>*);
+    auto _SetRot = (SetRot)(this->VTable[27]);
+
+    return _SetRot(this, &rot);
+
+};
+
+
+//
+
+
+auto Actor::isFireImmune(void) -> bool {
+
+    using IsFireImmune = bool (__thiscall*)(Actor*);
+    auto _IsFireImmune = (IsFireImmune)(this->VTable[39]);
+
+    return _IsFireImmune(this);
+
+};
+
+auto Actor::canDisableShield(void) -> bool {
+
+    using CanDisableShield = bool (__thiscall*)(Actor*);
+    auto _CanDisableShield = (CanDisableShield)(this->VTable[42]);
+
+    return _CanDisableShield(this);
+
+};
+
+
+//
+
+
+auto Actor::teleportTo(Vec3<float> pos, bool p2, int p3, int p4, bool p5) -> void {
+
+    using TeleportTo = void (__thiscall*)(Actor*, Vec3<float>*, bool, int, int, bool);
+    auto _TeleportTo = (TeleportTo)(this->VTable[43]);
+
+    return _TeleportTo(this, &pos, p2, p3, p4, p5);
+
+};
+
+auto Actor::tryTeleportTo(Vec3<float> pos, bool p2, bool p3, int p4, int p5) -> void {
+
+    using TryTeleportTo = void (__thiscall*)(Actor*, Vec3<float>*, bool, bool, int, int);
+    auto _TryTeleportTo = (TryTeleportTo)(this->VTable[44]);
+
+    return _TryTeleportTo(this, &pos, p2, p3, p4, p5);
+
+};
+
+auto Actor::chorusFruitTeleport(Vec3<float> pos) -> void {
+
+    using ChorusFruitTeleport = void (__thiscall*)(Actor*, Vec3<float>*);
+    auto _ChorusFruitTeleport = (ChorusFruitTeleport)(this->VTable[45]);
+
+    return _ChorusFruitTeleport(this, &pos);
+
+};
+
+
+//
+
+
+auto Actor::lerpMotion(Vec3<float> motion) -> void {
+
+    using LerpMotion = void (__thiscall*)(Actor*, Vec3<float>*);
+    auto _LerpMotion = (LerpMotion)(this->VTable[46]);
+
+    return _LerpMotion(this, &motion);
+
+};
+
+
+//
+
+
+auto Actor::getEntityTypeId(void) -> uint8_t {
+
+    using GetEntityTypeId = uint8_t (__thiscall*)(Actor*);
+    auto _GetEntityTypeId = (GetEntityTypeId)(this->VTable[169]);
+
+    return _GetEntityTypeId(this);
 
 };
