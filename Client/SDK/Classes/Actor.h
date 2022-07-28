10d6487a56c66e89ee1fcedbe350879bb4b37748
EchoHackCmd <33047114+EchoHackCmd@users.noreply.github.com>
2022-07-28 04:25:54 +0100

    Update Actor.h

--- Client/SDK/Classes/Actor.h
+++ Client/SDK/Classes/Actor.h
@@ -11,6 +11,20 @@ public:
 public:
     auto setPos(Vec3<float>) -> void;
     auto getPos(void) -> Vec3<float>;
+public:
+    auto getFiringPos(void) -> Vec3<float>;
+    auto setRot(Vec2<float>) -> void;
+public:
+    auto isFireImmune(void) -> bool;
+    auto canDisableShield(void) -> bool;
+public:
+    auto teleportTo(Vec3<float>, bool, int, int, bool) -> void;
+    auto tryTeleportTo(Vec3<float>, bool, bool, int, int) -> void;
+    auto chorusFruitTeleport(Vec3<float>) -> void;
+public:
+    auto lerpMotion(Vec3<float>) -> void;
+public:
+    auto getEntityTypeId(void) -> uint8_t;
 };
 
 #endif /* CLIENT_SDK_CLASSES_ACTOR */
