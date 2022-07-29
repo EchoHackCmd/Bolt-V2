dc50e40f83c2114c2ad4061eac47899d80318148
EchoHackCmd <33047114+EchoHackCmd@users.noreply.github.com>
2022-07-29 21:51:42 +0100

    Update Twerk.cpp

--- Client/Bolt/Module/Movement/Twerk.cpp
+++ Client/Bolt/Module/Movement/Twerk.cpp
@@ -5,41 +5,14 @@
 #include "../../Manager.h"
 #include "../../../Client.h"
 
-auto Twerk::onTick(void) -> void {
- 
- auto instance = Minecraft::getClientInstance();
-
-   auto player = (instance != nullptr ? instance->getLocalPlayer() : nullptr);
-       auto mcGame = (instance != nullptr ? instance->getMinecraftGame() : nullptr);
-
-
-    //
-  
-player->setSneaking(true);
-player->setSneaking(false);
-};
-
-auto Twerk::onEnable(void) -> void {
-
-    Utils::debugLog("Enabled: " + this->name);
-
-};
-
-auto Twerk::onDisable(void) -> void {
-
-    Utils::debugLog("Disabled: " + this->name);
-    
-    this->category->manager->isRunning = false;
-
-};
-
-auto Twerk::onRender(void) -> void {
-
-
-};
-
 auto Twerk::onGameMode(GameMode* GM) -> void {
- 
-  
- 
+    
+    auto player = GM->player;
+    
+    if(player == nullptr)
+        return this->setState(false);
+    
+    player->setSneaking(true);
+    player->setSneaking(false);
+
 };
