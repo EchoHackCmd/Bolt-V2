bb31a321dce38f6d603fa6fb42e0ec4616f18ecf
EchoHackCmd <33047114+EchoHackCmd@users.noreply.github.com>
2022-07-29 17:28:08 +0100

    Update Criticals.cpp

--- Client/Bolt/Module/Combat/Criticals.cpp
+++ Client/Bolt/Module/Combat/Criticals.cpp
@@ -1,19 +1,25 @@
 #include "Criticals.h"
-#include "../../../Utils/Utils.h"
-#include "../../Category/Category.h"
-#include "../../Manager.h"
-#include "../../../Client.h"
 
-void* criticalsAddress = (void*)(Utils::findSig("73 5A 80 B9 D8 01 ? ? ? 75 51"));
+#include "../../Category/Category.h"
+#include "../../../Client.h"
+#include "../../Manager.h"
+
+auto criticalsAddress = (void*)(Utils::findSig("73 5A 80 B9 D8 01 ? ? ? 75 51"));
 char ogCrits[9] = {};
 
 auto Criticals::onEnable(void) -> void {
+
 	if (criticalsAddress != nullptr) {
-	Utils::copyBytes(criticalsAddress, ogCrits, 9);
-	Utils::nopBytes(criticalsAddress, 9);
-	}
-}
+		
+		Utils::copyBytes(criticalsAddress, ogCrits, 9);
+		Utils::nopBytes(criticalsAddress, 9);
+	
+	};
+
+};
 
 auto Criticals::onDisable(void) -> void{
+	
 	Utils::patchBytes(criticalsAddress, ogCrits, 9);
-}
+	
+};
