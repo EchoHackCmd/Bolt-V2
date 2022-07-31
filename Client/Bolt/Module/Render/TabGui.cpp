43562eb1efc2f87390793953be70f1de837746db
EchoHackCmd <33047114+EchoHackCmd@users.noreply.github.com>
2022-07-31 04:27:08 +0100

    Update TabGui.cpp

--- Client/Bolt/Module/Render/TabGui.cpp
+++ Client/Bolt/Module/Render/TabGui.cpp
@@ -6,7 +6,13 @@
 
 auto TabGui::onRender(void) -> void {
 
-    //
+    this->updateAlpha();
+
+    auto textColor = ImColor(34.f, 201.f, 126.f, this->alpha);
+    auto bgColor = ImColor(28.f, 28.f, 28.f, this->alpha);
+    
+    auto mgr = this->category->manager;
+    auto categories = mgr->categories;
 
 };
 
@@ -14,4 +20,30 @@ auto TabGui::onKey(uint64_t key, bool isDown, bool* cancel) -> void {
 
     //
 
+};
+
+auto TabGui::updateAlpha(void) -> void {
+
+    auto reachOff = [&](float* value, float target, float modifier) {
+        if(*value < target)
+            *value += modifier;
+        else
+            *value -= modifier;
+    };
+
+    auto instance = Minecraft::getClientInstance();
+    
+    auto player = (instance != nullptr ? instance->getLocalPlayer() : nullptr);
+    auto mcGame = (instance != nullptr ? instance->getMinecraftGame() : nullptr);
+    
+    auto modifier = 0.04f;
+    
+    if(mcGame == nullptr || player == nullptr)
+        return reachOff(&alpha, 1.f, modifier);
+    
+    if(mcGame->canUseKeys())
+        return reachOff(&alpha, 1.f, modifier);
+    else
+        return reachOff(&alpha, .0f, modifier);
+    
 };
