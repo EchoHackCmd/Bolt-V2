4369abb55da5b8eaa6464d137d71342761ff6770
EchoHackCmd <33047114+EchoHackCmd@users.noreply.github.com>
2022-08-04 13:12:03 +0100

    Create Killaura.cpp

---
+++ Client/Bolt/Module/Combat/Killaura.cpp
@@ -0,0 +1,60 @@
+#include "Killaura.h"
+
+#include "../../Manager.h"
+#include "../../../Client.h"
+#include "../../Category/Category.h"
+
+auto Killaura::onGameMode(GameMode* GM) -> void {
+
+    auto player = GM->player;
+    
+    auto mgr = this->category->manager;
+    auto entityMap = mgr->entityMap;
+
+    auto distances = std::vector<float>();
+    
+    for(auto [ runtimeId, entity ] : entityMap) {
+
+        if(player->getRuntimeId() == runtimeId)
+            continue;
+        
+        if(!entity->isAlive() || !player->canAttack(entity, false))
+            continue;
+        
+        auto dist = player->getPos().distanceTo(entity->getPos());
+        
+        if(dist <= this->range)
+            distances.push_back(dist);
+
+    };
+
+    if(distances.empty())
+        return;
+    
+    std::sort(distances.begin(), distances.end());
+    auto I = 0;
+
+    for(auto [ runtimeId, entity ] : entityMap) {
+
+        if(I >= 2)
+            break;
+
+        if(player->getRuntimeId() == runtimeId)
+            continue;
+        
+        if(!entity->isAlive() || !player->canAttack(entity, false))
+            continue;
+        
+        auto dist = player->getPos().distanceTo(entity->getPos());
+        
+        if(dist == distances.at(0) || dist == (distances.size() > 1 ? distances.at(1) : distances.at(0))) {
+
+            GM->attack(entity);
+            player->swing();
+            I++;
+
+        };
+
+    };
+
+};
