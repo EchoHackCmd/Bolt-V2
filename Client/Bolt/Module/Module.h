85bc02674f46b02eb330cc7d65db744678573864
EchoHackCmd <33047114+EchoHackCmd@users.noreply.github.com>
2022-08-05 19:17:34 +0100

    Update Module.h

--- Client/Bolt/Module/Module.h
+++ Client/Bolt/Module/Module.h
@@ -14,6 +14,8 @@ public:
     uint64_t key;
     bool isEnabled = false, wasEnabled = false;
 public:
+    std::map<uintptr_t, std::vector<BYTE>> patches;
+public:
     Module(std::string, Category*);
 public:
     auto setState(bool) -> void;
@@ -28,6 +30,47 @@ public:
     virtual auto onKey(uint64_t, bool, bool*) -> void {};
 public:
     virtual auto onGamma(float*) -> void {};
+public:
+    auto getBytes(void* src, unsigned int bytes) -> std::vector<BYTE> {
+
+        auto res = std::vector<BYTE>();
+
+        for(auto I = 0; I < bytes; I++) {
+
+            res.push_back(*(BYTE*)((uintptr_t)(src) + I));
+
+        };
+
+        return res;
+
+    };
+
+    auto nopBytes(void* src, unsigned int bytes) -> void {
+
+        DWORD oldprotect;
+        VirtualProtect(src, bytes, PAGE_EXECUTE_READWRITE, &oldprotect);
+        
+        memset(src, 0x90, bytes);
+
+        VirtualProtect(src, bytes, oldprotect, &oldprotect);
+
+    };
+
+    auto patchBytes(void* src, std::vector<BYTE> bytes) -> void {
+
+        DWORD oldprotect;
+        
+        VirtualProtect(src, bytes.size(), PAGE_EXECUTE_READWRITE, &oldprotect);
+
+        for(auto I = 0; I < bytes.size(); I++) {
+
+            *(BYTE*)((uintptr_t)(src) + I) = bytes.at(I);
+
+        };
+        
+        VirtualProtect(src, bytes.size(), oldprotect, &oldprotect);
+
+    };
 };
 
 #endif /* CLIENT_BOLT_MODULE_MODULE */
