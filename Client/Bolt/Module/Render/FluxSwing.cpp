c4e407328f475d7733f2965a53ab783cece3eeb9
EchoHackCmd <33047114+EchoHackCmd@users.noreply.github.com>
2022-08-05 19:17:30 +0100

    Update FluxSwing.cpp

--- Client/Bolt/Module/Render/FluxSwing.cpp
+++ Client/Bolt/Module/Render/FluxSwing.cpp
@@ -4,20 +4,31 @@
 #include "../../../Client.h"
 #include "../../Manager.h"
 
-void* fluxSwingAddr = (void*)(Utils::findSig("e8 ? ? ? ? 41 0f 28 c0 f3 41 0f 5c c1"));
-char ogFlux[5] = {};
-
 auto FluxSwing::onEnable(void) -> void {
-	if (fluxSwingAddr == nullptr)
-		return;
-	
-	Utils::copyBytes(fluxSwingAddr, ogFlux, 5);
-	Utils::nopBytes(fluxSwingAddr, 5);	
-	
+
+	if(this->patches.empty()) {
+
+		auto fluxSwingAddr = Utils::findSig("E8 ? ? ? ? 41 0F 28 C0 F3 41 0F 5C C1");
+		auto bytes = getBytes((void*)fluxSwingAddr, 5);
+
+		this->patches[fluxSwingAddr] = bytes;
+
+	};
+
+	for(auto [ addr, bytes ] : this->patches) {
+
+		nopBytes((void*)addr, bytes.size());
+
+	};
+
 };
 
-auto FluxSwing::onDisable(void) -> void{
-	
-	Utils::patchBytes(fluxSwingAddr, ogFlux, 5);
+auto FluxSwing::onDisable(void) -> void {
+
+	for(auto [ addr, bytes ] : this->patches) {
+
+		patchBytes((void*)addr, bytes);
+
+	};
 
 };
