7bd7b1d417e41925829c6b7a88b7f33176181dc2
EchoHackCmd <33047114+EchoHackCmd@users.noreply.github.com>
2022-08-06 02:20:09 +0100

    Update Module.cpp

--- Client/Bolt/Module/Module.cpp
+++ Client/Bolt/Module/Module.cpp
@@ -22,3 +22,47 @@ auto Module::setKey(uint64_t key) -> void {
     this->key = key;
 
 };
+
+
+/* Module Mem Patching */
+
+auto Module::getBytes(void* src, unsigned int bytes) -> std::vector<BYTE> {
+
+    auto res = std::vector<BYTE>();
+
+    for(auto I = 0; I < bytes; I++) {
+
+        res.push_back(*(BYTE*)((uintptr_t)(src) + I));
+
+    };
+
+    return res;
+
+};
+
+auto Module::patchBytes(void* src, std::vector<BYTE> bytes) -> void {
+
+    DWORD oldprotect;
+    
+    VirtualProtect(src, bytes.size(), PAGE_EXECUTE_READWRITE, &oldprotect);
+
+    for(auto I = 0; I < bytes.size(); I++) {
+
+        *(BYTE*)((uintptr_t)(src) + I) = bytes.at(I);
+
+    };
+    
+    VirtualProtect(src, bytes.size(), oldprotect, &oldprotect);
+
+};
+
+auto Module::nopBytes(void* src, unsigned int bytes) -> void {
+
+    DWORD oldprotect;
+    VirtualProtect(src, bytes, PAGE_EXECUTE_READWRITE, &oldprotect);
+    
+    memset(src, 0x90, bytes);
+
+    VirtualProtect(src, bytes, oldprotect, &oldprotect);
+
+};
