b09b060b258f982d3cb034d89babac454314fef8
EchoHackCmd <33047114+EchoHackCmd@users.noreply.github.com>
2022-08-06 06:29:34 +0100

    Update PlayerInventory.h

--- Client/SDK/Classes/PlayerInventory.h
+++ Client/SDK/Classes/PlayerInventory.h
@@ -19,11 +19,16 @@ private:
     virtual auto Function4(void) -> void;
     virtual auto getContainerWeakRef(void) -> void*;
 public:
-    virtual auto getItemStack(int) -> class ItemStack*;
+    virtual auto getItemStack(int) -> ItemStack*;
 private:
     virtual auto getTagRegistry(void) -> void*;
 public:
-    virtual auto getItemInstance(int) -> class ItemInstance*;
+    virtual auto getItemInstance(int) -> ItemInstance*;
+private:
+    virtual auto Function9(void) -> void;
+    virtual auto Function10(void) -> void;
+public:
+    virtual auto setItem(int, ItemStack*) -> void;
 };
 
 #endif /* CLIENT_SDK_CLASSES_PLAYERINVENTORY */
