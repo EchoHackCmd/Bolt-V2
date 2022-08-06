cf38b5145a99f29afd1a3250aa01184f986615b1
EchoHackCmd <33047114+EchoHackCmd@users.noreply.github.com>
2022-08-06 06:18:46 +0100

    Update Player.h

--- Client/SDK/Classes/Player.h
+++ Client/SDK/Classes/Player.h
@@ -2,10 +2,11 @@
 #define CLIENT_SDK_CLASSES_PLAYER
 
 #include "Actor.h"
+#include "PlayerInventory.h"
 
 class Player : public Actor {
 public:
-    //
+    auto getSupplies(void) -> class PlayerInventory*;
 };
 
 #endif /* CLIENT_SDK_CLASSES_PLAYER */
