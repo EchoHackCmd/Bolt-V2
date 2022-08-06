3fc232225000de89a8ab984e170f6a4e8608889b
EchoHackCmd <33047114+EchoHackCmd@users.noreply.github.com>
2022-08-06 18:09:08 +0100

    Update MinecraftUIRenderContext.cpp

--- Client/SDK/Classes/MinecraftUIRenderContext.cpp
+++ Client/SDK/Classes/MinecraftUIRenderContext.cpp
@@ -36,6 +36,22 @@ auto MCRenderer::getFont(void) -> Font* {
 //
 
 
+auto MCRenderer::getTextWidth(std::string text, float fontSize) -> float {
+
+    auto ctx = MCRenderer::getContext();
+    auto font = MCRenderer::getFont();
+
+    if(ctx == nullptr || font == nullptr)
+        return 0.f;
+    
+    return ctx->getLineLength(font, &text, fontSize, false);
+
+};
+
+
+//
+
+
 auto MCRenderer::drawText(std::string text, Vec2<float> textPos, Color color, float fontSize) -> void {
 
     auto ctx = MCRenderer::getContext();
@@ -49,5 +65,42 @@ auto MCRenderer::drawText(std::string text, Vec2<float> textPos, Color color, fl
 
     Rect rect = Rect(textPos.x, textPos.x + (textPos.x * fontSize), textPos.y, textPos.y + (textPos.y * fontSize / 2));
     ctx->drawText(font, &rect, &text, &color, color.w * 255.f, nullptr, &textMeasureData, &caretMeasureData);
+
+};
+
+auto MCRenderer::flushText(void) -> void {
+
+    auto ctx = MCRenderer::getContext();
+
+    if(ctx == nullptr)
+        return;
+    
+    return ctx->flushText(0.f);
+
+};
+
+
+//
+
+
+auto MCRenderer::fillRectangle(Vec4<float> rectPos, Color rectColor) -> void {
+
+    auto ctx = MCRenderer::getContext();
+
+    if(ctx == nullptr)
+        return;
+    
+    ctx->fillRectangle(Vec4<float>(rectPos.x, rectPos.z, rectPos.y, rectPos.w), rectColor, rectColor.w * 255.f);
+
+};
+
+auto MCRenderer::drawRectangle(Vec4<float> rectPos, Color rectColor, int rectLineWidth) -> void {
+
+    auto ctx = MCRenderer::getContext();
+
+    if(ctx == nullptr)
+        return;
+    
+    ctx->drawRectangle(Vec4<float>(rectPos.x, rectPos.z, rectPos.y, rectPos.w), rectColor, rectColor.w * 255.f, rectLineWidth);
 
 };
