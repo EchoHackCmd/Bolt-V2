d0d32f6e4988a4b6a24e1ca8fa72c80da58bb08c
EchoHackCmd <33047114+EchoHackCmd@users.noreply.github.com>
2022-08-07 16:29:01 +0100

    Thanks Floppy

--- Client/SDK/Classes/Inventory.h
+++ Client/SDK/Classes/Inventory.h
@@ -7,6 +7,11 @@ struct ContainerContentChangeListener;
 class ItemStack;
 class BlockSource;
 class Item;
+class Player;
+class ItemInstance;
+struct CompoundTag;
+struct ListTag;
+
 class Inventory {
 private:
     virtual ~Inventory(void);
@@ -15,7 +20,6 @@ public:
     virtual auto serverInitItemStackIds(int, int, class std::function<void (int, class ItemStack const &)>) -> void;
     virtual auto addContentChangeListener(ContainerContentChangeListener*) -> void;
     virtual auto removeContentChangeListener(ContainerContentChangeListener*) -> void;
-public:
     virtual auto getItem(int) -> ItemStack*;
     virtual auto hasRoomForItem(ItemStack*) -> bool;
     virtual auto addItem(ItemStack*) -> bool;
@@ -27,41 +31,28 @@ public:
     virtual auto dropContents(BlockSource*, Vec3<float>, bool) -> void;
     virtual auto getContainerSize(void) -> __int64;
     virtual auto getMaxStackSize(void) -> __int64;
-private:
-    virtual auto Function15(void) -> void;
-    virtual auto Function16(void) -> void;
-public:
+    virtual auto startOpen(class Player*) -> void;
+    virtual auto stopOpen(class Player*) -> void;
     virtual auto getSlotCopies(void) -> std::vector<ItemStack>;
     virtual auto getSlots(void) -> std::vector<ItemStack*>;
-public:
     virtual auto getItemCount(ItemStack*) -> int;
     virtual auto findFirstSlotForItem(ItemStack*) -> int;
-private:
-    virtual auto Function21(void) -> void;
-    virtual auto Function22(void) -> void;
-public:
+    virtual auto canPushInItem(BlockSource &, int32_t, int32_t, ItemInstance const&) -> bool;
+    virtual auto canPullOutItem(BlockSource &,int32_t,int32_t,ItemInstance const&) -> bool;
     virtual auto setContainerChanged(int) -> void;
     virtual auto setContainerMoved(void) -> void;
-public:
     virtual auto setCustomName(std::string) -> void;
-public:
-    virtual auto isWatchingIfActorEnters(void) -> bool;
-private:
-    virtual auto Function27(void) -> void;
-    virtual auto Function28(void) -> void;
-public:
+    virtual auto hasCustomName(void) -> bool;
+    virtual auto readAdditionalSaveData(CompoundTag const&) -> void;
+    virtual auto addAdditionalSaveData(CompoundTag&) -> void;
     virtual auto createTransactionContext(std::function<void (Inventory &, int, ItemStack const &, ItemStack const &)>,std::function<void (void)>) -> void;
-private:
-    virtual auto Function30(void) -> void;
-public:
+    virtual auto initializeContainerContents(BlockSource &) -> void;
     virtual auto isEmpty(void) -> bool;
     virtual auto add(ItemStack*) -> bool;
     virtual auto canAdd(ItemStack*) -> bool;
     virtual auto clearSlot(int) -> void;
     virtual auto clearInventory(int) -> void;
-private:
-    virtual auto Function36(void) -> void;
-public:
+    virtual auto load(ListTag const&, void* const&, Level &) -> void;
     virtual auto getEmptySlotsCount(void) -> __int64;
     virtual auto getFirstEmptySlot(void) -> __int64;
     virtual auto setContainerSize(int) -> void;
