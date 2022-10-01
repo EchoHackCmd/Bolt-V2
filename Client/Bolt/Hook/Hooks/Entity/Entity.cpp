4193b6caae6e0b9c0393bf3e19edcb40ceaca1f3
EchoHackCmd <EchoHackCmd@mail.com>
2022-10-01 06:27:38 +0100

    Update Entity.cpp

--- Client/Bolt/Hook/Hooks/Entity/Entity.cpp
+++ Client/Bolt/Hook/Hooks/Entity/Entity.cpp
@@ -4,15 +4,15 @@
 
 Manager* ehMgr = nullptr;
 
-typedef void (__thiscall* EntityTick)(Actor*, void*, void*);
+typedef void (__thiscall* EntityTick)(Actor*, void*);
 EntityTick _EntityTick;
 
-auto EntityTick_Callback(Actor* entity, void* p2, void* p3) -> void {
+auto EntityTick_Callback(Actor* entity, void* p2) -> void {
 
     ehMgr->cleanEntityMap();
     ehMgr->entityMap[entity->getRuntimeId()] = entity;
     
-    _EntityTick(entity, p2, p3);
+    _EntityTick(entity, p2);
 
 };
 
@@ -20,7 +20,7 @@ auto Entity_Hook::init(void) -> void {
 
     ehMgr = this->manager;
 
-    auto sig = Utils::findSig("0F B6 81 ? ? ? ? C3 ? ? ? ? ? ? ? ? 48 8B 91 ? ? ? ? 0F");
+    auto sig = Utils::findSig("48 89 5C 24 ? 48 89 6C 24 ? 56 57 41 54 41 56 41 57 48 83 EC 60 48 8B 05 ? ? ? ? 48 33 C4 48 89 44 24 ? 4C 8B E2 48 8B"); /* Minecraft Functions suck */
 
     if(!sig)
         return Utils::debugLog("[ Entity Tick Hook ] Failed to find Signature needed for Hook!");
