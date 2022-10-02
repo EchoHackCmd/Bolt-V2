fd45e9f4451be5901e00092305fe9471e0cdb660
EchoHackCmd <EchoHackCmd@mail.com>
2022-10-02 09:08:20 +0100

    Update Killaura.h

--- Client/Bolt/Module/Combat/Killaura.h
+++ Client/Bolt/Module/Combat/Killaura.h
@@ -11,7 +11,11 @@ public:
 public:
     auto onGameMode(GameMode*) -> void override;
 private:
-    float range = 12.f;
+    float range = 12.f; /*       Range in proximity of attacks        */
+    int cpt = 2;        /*       Clicks per tick                      */
+private:
+    std::chrono::time_point<std::chrono::system_clock> time = std::chrono::system_clock::now();
+    int msDelay = 50;
 };
 
 #endif /* CLIENT_BOLT_MODULE_COMBAT_KILLAURA */
